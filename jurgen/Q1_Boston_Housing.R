70cbcc218fc32228e12ac8c5b56556507e39ca40
sune Jepsen <kongsune@hotmail.com>
2017-12-13 19:56:45 +0100

    Worked on assignment 1

--- jurgen/Q1_Boston_Housing.R
+++ jurgen/Q1_Boston_Housing.R
@@ -17,6 +17,7 @@ data("Boston")
 # Create list with same size as crime coloum with all zeros
 crim01 <- rep(0, length(Boston$crim))
 
+
 # Insert 1's where crime is over median 
 crim01[Boston$crim > median(Boston$crim)] <- 1
 
@@ -32,7 +33,7 @@ set.seed(1234)
 train <- sample(1:dim(Boston)[1], dim(Boston)[1]*.7, rep=FALSE)
 
 # Create test data (-train means all not all train data)
- test <- -train
+test <- -train
   
 # Add training data to variable
 Boston.train <- Boston[train, ]
@@ -50,7 +51,9 @@ crim01.test <- crim01[test]
   # - crime01 -crim: remove crime01 and crim colums as predictors
   # data = Boston: use Boston data set
   # binomial: logit
-fit.glm1 <- glm(crim01 ~ . - crim01 - crim, data = Boston, family = binomial)
+#fit.glm1 <- glm(crim01 ~ . -crim, family = binomial(logit), data = Boston)
+#fit.glm1 <- glm(crim01 ~ -crim + zn + indus + chas + nox + rm + age + dis + rad + tax + ptratio + black + lstat + medv + rvYes, family = binomial(logit), data = Boston)
+fit.glm1 <- glm(crim01 ~ -crim + zn + indus + chas + nox + rm + age + dis + rad + tax + ptratio + black + lstat + medv, family = binomial(logit), data = Boston)
 
 # Summary of the models coefficients and p-values
 summary(fit.glm1)
@@ -63,8 +66,16 @@ corrplot::corrplot.mixed(cor(Boston[, -1]), upper="circle")
   # GLM Parameter explaination
   # crim01: The variable we want to predict
   # nox (0.72) + indus (0.6) + age (0.61) + rad (0.62): Predictors 
-fit.glm <- glm(crim01 ~ nox + indus + age + rad, data = Boston, family = binomial)
-Summary(fit.glm)
+  # 
+fit.glm <- glm(crim01 ~ -crim + dis + zn + nox + medv + rad + tax + ptratio + black, data = Boston.train, family = binomial)
+
+# 0.01
+#fit.glm <- glm(crim01 ~ dis  + nox  + rad  + ptratio, data = Boston.train, family = binomial)
+
+# 0.001
+#fit.glm <- glm(crim01 ~  nox  + rad, data = Boston.train, family = binomial)
+
+summary(fit.glm)
 
 
 # Use our model and see how it predicts on Test data set 
@@ -82,19 +93,39 @@ pred.glm[probs > 0.5] <- 1
   # wrong prediction: 15 which the model predicted to no crime had crime. 4 which the model predicted had crime which hadnt crime
 table(pred.glm, crim01.test)
 
-# Computes the rate of misclassification (0.125)
+# Computes the rate of misclassification (0.112)
 mean(pred.glm != crim01.test)
 
 ##############################################################################
 # Linear discriminant analysis
 ##############################################################################
+#crim01
 
+#crim01 <- factor(crim01)
 
+str(crim01)
 # Create model
-fit.lda <- lda(crim01 ~ nox + indus + age + rad , data = Boston)
+#fit.lda <- lda(crim01 ~., data = Boston)
+
+#Use the p values to determine which variable to sufficient for the model training
+
+#LDA Model with the training data
+# 0.05 significants level
+fit.lda.train <- lda(crim01 ~ -crim + dis + zn + nox + medv + rad + tax + ptratio + black, data = Boston.train)
+
+#0.01
+#fit.lda.train <- lda(crim01 ~ -crim + dis  + nox  + rad  + ptratio, data = Boston.train)
+
+#0.001
+#fit.lda.train <- lda(crim01 ~ -crim + nox  + rad, data = Boston.train )
+
+fit.lda.train
+
 
 # Use the model for prediction on test data
-pred.lda <- predict(fit.lda, Boston.test)
+pred.lda <- predict(fit.lda.train, Boston.test)
+
+
 
 # Confusion matrix
 table(pred.lda$class, crim01.test)
@@ -106,45 +137,96 @@ mean(pred.lda$class != crim01.test)
 ##############################################################################
 # KNN
 ##############################################################################
-
-data = scale(Boston[,-c(1,15)])
-set.seed(1234)
-train <- sample(1:dim(Boston)[1], dim(Boston)[1]*.7, rep=FALSE)
-test <- -train
-training_data = data[train, c("nox" , "indus" , "age" , "rad")]
-testing_data = data[test, c("nox" , "indus" , "age" , "rad")]
-## KNN take the training response variable seperately
-train.crime01 = Boston$crim01[train]
-## we also need the have the testing_y seperately for assesing the model later on
-test.crime01= Boston$crim01[test]
-
 library(class)
-set.seed(1234)
-knn_pred_y = knn(training_data, testing_data, train.crime01, k = 1)
-table(knn_pred_y, test.crime01)
 
-# For this KNN (k=1), we have a test error rate of 9.21%
-mean(knn_pred_y != test.crime01)
 
-knn_pred_y = NULL
-error_rate = NULL
-for(i in 1:dim(testing_data)[1]){
-  set.seed(1234)
-  knn_pred_y = knn(training_data,testing_data,train.crime01,k=i)
-  error_rate[i] = mean(test.crime01 != knn_pred_y)
+
+normalize <- function(x) {
+  return ((x - min(x)) / (max(x) - min(x)))
 }
 
-### find the minimum error rate
+#data = scale(Boston[,-c(1,15)])
+
+# Nomalize data
+data <- as.data.frame(lapply(Boston[,2:14], normalize))
+
+# Set random generator
+set.seed(1234)
+
+# Create training data - 70% training  data and 30% test data
+train <- sample(1:dim(Boston)[1], dim(Boston)[1]*.70, rep=FALSE)
+
+# Create test data (-train means all not all train data)
+test <- -train
+
+# 5% significance
+#training_data = data[train, c("nox" , "indus" , "age" , "rad", "dis", "zn", "tax", "ptratio")]
+
+# 1% significance
+#training_data = data[train, c("nox" ,  "rad", "dis",  "ptratio")]
+
+# 0.1% significance
+training_data = data[train, c("nox" ,  "rad")]
+
+# 5% significance
+#testing_data = data[test, c("nox" , "indus" , "age" , "rad", "dis", "zn", "tax", "ptratio")]
+
+# 1% significance
+#testing_data = data[test, c("nox" ,  "rad", "dis",  "ptratio")]
+
+# 0.1% significance
+testing_data = data[test, c("nox" ,  "rad")]
+
+
+# Add crime01 to train.crim01
+train.crim01 = Boston$crim01[train]
+
+# Add crime01 to test.crime01
+test.crim01= Boston$crim01[test]
+
+# Create list for accuracy for each K
+acc_ls <- list()
+
+# accuracy function
+accuracy  <- function(x,y){
+  acc <- 0
+  
+  for(i in 1:length(y)){
+    if(y[i] == x[i]){
+      acc <- acc + 1
+    }
+  }
+  return(100*acc/length(y))
+  
+}
+
+# Run prediction for each k value and get the accuracy
+# Unlike LDA and logistic regression we do not fit and validate in two steps
+# In KNN we both in one step
+k <- 1:35
+error_rate = NULL
+for(i in k){
+  pred_k <- knn(Boston.train, Boston.test, train.crim01, i)
+  acc_ls[i] <- accuracy(pred_k,test.crim01)
+  error_rate[i] = mean(test.crim01 != pred_k)
+}
+
+# find the minimum error rate for all k's
 min_error_rate = min(error_rate)
 print(min_error_rate)
 
-### get the index of that error rate, which is the k
-K = which(error_rate == min_error_rate)
-print(K)
+# Average of all accuracies
+mean(unlist(acc_ls))
 
-# Minimum error rate is 6.57% at K=3
+# Plot the accuracy of all k's
+plot(k,acc_ls)
 
-library(ggplot2)
-qplot(1:dim(testing_data)[1], error_rate, xlab = "K",
-      ylab = "Error Rate",
-      geom=c("point", "line"))
+# KNN training in respect to subset and k.
+  # From the prediction of finding the best k with best accuracy, we get k=3 and k=5
+  # The outcome is a prediction model on the testting_data
+knn_pred_y = knn(training_data, testing_data, train.crime01, k = 5)
+
+table(knn_pred_y, test.crime01)
+
+# For this KNN (k=3), we have a test error rate of ...
+mean(knn_pred_y != test.crime01)
